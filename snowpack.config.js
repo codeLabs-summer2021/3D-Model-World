93431b9a274315258fdd3e00ca12665265af4e98
Caleb Mcolin <Caleb.mcolin@gmail.com>
2021-07-15 12:44:54 -0700

    adding webpack for build server

---
+++ snowpack.config.js
@@ -0,0 +1,21 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+export const mount = {
+  /* ... */
+};
+export const plugins = [
+  [
+    '@snowpack/plugin-webpack',
+    {
+      /* Plugin Options (see https://www.npmjs.com/package/@snowpack/plugin-webpack) */  
+    },
+  ],
+];
+export const packageOptions = {
+  /* ... */
+};
+export const devOptions = {
+  /* ... */
+};
+export const buildOptions = {
+  /* ... */
+};

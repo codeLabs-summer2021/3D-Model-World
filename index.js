88b0dea9e7630f42d232e142518579e8b6b1d429
Caleb Mcolin <Caleb.mcolin@gmail.com>
2021-07-14 17:38:22 -0700

    addition of canvas-confetti (for testing)

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+import confetti from 'canvas-confetti';
+
+confetti.create(document.getElementById('canvas'), {
+    resize: true,
+    useWorker: true,
+})({ particleCount: 200, spread: 200 });
+
 console.log('js is working');

056eb19faaf522bd85ccfdba454d3ea61dad11e6
Caleb Mcolin <Caleb.mcolin@gmail.com>
2021-07-21 15:44:04 -0700

    Added mapbox-gl/Removed canvas-confetti/Set up map

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-import confetti from 'canvas-confetti';
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-confetti.create(document.getElementById('canvas'), {
-    resize: true,
-    useWorker: true,
-})({ particleCount: 200, spread: 200 });
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2FsZWJtYyIsImEiOiJja3F1ZGh4eDgwM2pzMnBwYngwdHk4anNoIn0.ynFiLgiuvax1jiCqEozo_A';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y',
+    center: [-80.61, 28.6123],
+    zoom: 15,
+    pitch: 60,
+    antialias: true
+});
 
 console.log('js is working');

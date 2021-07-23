6f76cacbb5d7699f760f510e746aca4b52fd9100
randrade8311 <randrade709@gmail.com>
2021-07-23 14:10:45 -0700

    Added authentication from users to be able to add models from sketchfab
    - Code will display models from zipped folder with only glb file
    - Code does not yet display items from gltf folder

---
+++ SketchfabIntegration.js
@@ -0,0 +1,123 @@
+const CLIENT_ID = 'qzsZjdRBnwZhC51TYPgsLcfrl2RpeoZKVBpexr8J';
+const AUTHENTICATION_URL = `https://sketchfab.com/oauth2/authorize/?state=123456789&response_type=token&client_id=${CLIENT_ID}`;
+import JSZip from "jszip";
+
+function checkStatus(response) {
+    // From: https://gist.github.com/irbull/42f3bd7a9db767ce72a770ded9a5bdd1
+    if (!response.ok) {
+        throw new Error(`HTTP ${response.status} - ${response.statusText}`);
+    }
+    return response;
+}
+
+function getExtension(filename) {
+    return filename.toLowerCase().split('.').pop();
+}
+
+async function getFileUrl(file) {
+    const blob = await file.async('blob');
+    const url = URL.createObjectURL(blob);
+    return url;
+}
+
+class SketchfabIntegration {
+    constructor() {
+        this.token = null;
+    }
+
+    authenticate() {
+        window.open(AUTHENTICATION_URL, '_blank');
+    }
+
+    async readZip(zipUrl) {
+        const response = await fetch(zipUrl);
+        checkStatus(response);
+        const arrayBuffer = await response.arrayBuffer();
+
+        const result = await JSZip.loadAsync(arrayBuffer);
+
+        const files = Object.values(result.files).filter(item => !item.dir);
+        // This line will load files with the glb extension
+        const entryFile = files.find(f => getExtension(f.name) === 'glb');
+        // Code to load gltf files
+        // const entryFile = files.find(f => getExtension(f.name) === 'gltf');
+        // Create blobs for every file resource
+        const blobUrls = {};
+        for (const file of files) {
+            console.log(typeof file);
+            if (typeof file !== "undefined") {
+                console.log(`Loading ${file.name}...`);
+                blobUrls[file.name] = await getFileUrl(file);
+            }
+        }
+        const fileUrl = blobUrls[entryFile.name];
+        return fileUrl;
+    }
+
+    checkToken() {
+        const url = new URL(window.location);
+        // Extract the token and save it
+        const hashParams = url.hash.split('&');
+        for (let param of hashParams) {
+            if (param.indexOf("access_token") !== -1) {
+                const token = param.replace('#access_token=', '');
+                console.log("Detected Sketchfab token: ", token);
+                localStorage.setItem("sb_token", token);
+            }
+        }
+
+        // Load token from local storage
+        this.token = localStorage.getItem("sb_token");
+    }
+
+    async getModelDownloadUrl(inputUrl) {
+        // Extract the model ID from the URL
+        const input = new URL(inputUrl);
+        // The ID is always the last string when seperating by '-'
+        const pieces = input.pathname.split('-');
+        const modelID = pieces[pieces.length - 1];
+
+        const metadataUrl = `https://api.sketchfab.com/v3/models/${modelID}/download`;
+        const options = {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${this.token}`,
+            },
+            mode: 'cors'
+        };
+
+        // This call will fail if model can't be downloaded
+        const response = await fetch(metadataUrl, options);
+        const metadata = await response.json();
+        return metadata.gltf.url;
+    }
+
+    async fetchModel(url) {
+        let modelZipUrl;
+        try {
+            modelZipUrl = await this.getModelDownloadUrl(url);
+        } catch (e) {
+            // Update modal with error
+            console.error("Failed to download model from Sketchfab", e);
+        }
+
+        if (modelZipUrl == undefined) return;
+
+        // Update modal with "Loading model"
+        // document.querySelector('#fetch-success').style.display = 'block';
+        console.log("success!!");
+        let finalURL;
+        try {
+            finalURL = await this.readZip(modelZipUrl);
+        } catch (e) {
+            // Update modal with error 
+            console.error("Failed to read model from Sketchfab", e);
+            return;
+        }
+
+        // Returns read zipped url
+        return finalURL;
+    }
+}
+
+export default SketchfabIntegration;
--- cesium.js
+++ cesium.js
@@ -3,6 +3,49 @@ window.CESIUM_BASE_URL = '/static/Cesium/';
 
 import * as Cesium from 'cesium';
 import "/static/Cesium/Widgets/widgets.css";
+import SketchfabIntegration from "./SketchfabIntegration.js";
+
+$("#authenticate").on("click", authenticateUser);
+$("#submitLink").on("click", getSketchfabModelUrl);
+$("#linkSketchfab").hide();
+$("#submitLink").hide();
+
+let modelgltfURL;
+
+const sketchfabIntegration = new SketchfabIntegration();
+sketchfabIntegration.checkToken();
+
+function authenticateUser() {
+    sketchfabIntegration.authenticate();
+    // After authentication, check for token 
+    sketchfabIntegration.checkToken();
+    // Token hardcoced for testing purposes
+    // sketchfabIntegration.token = "INSERT TOKEN FOR TESTING";
+    // sketchfabIntegration.checkToken();
+    if (sketchfabIntegration.token != null) {
+        $("#linkSketchfab").show();
+        $("#submitLink").show();
+    }
+    // console.log("Authenticating");
+}
+
+async function getSketchfabModelUrl() {
+    let modelURL = $("#linkSketchfab").val();
+    console.log(modelURL);
+    // Contains blob url
+    // Fetch model will load model from sketchfab link
+    // modelgltfURL = await sketchfabIntegration.fetchModel(modelURL);
+    // readzip only have link to glb file inside zip folder
+    modelgltfURL = await sketchfabIntegration.readZip("/static/Assets/electricalBox.glb.zip");
+    console.log(modelgltfURL);
+    // create model will create the model in cesium and display it 
+    createModel(modelgltfURL);
+
+    // {
+    // This code will load models if stored in a static folder as glbs which has the requirements for Cesium to load files
+    // createModel("/static/Assets/electricalBox.glb");
+    // }
+}
 
 // Your access token can be found at: https://cesium.com/ion/tokens.
 // This is the default access token from your ion account
@@ -23,3 +66,14 @@ viewer.camera.flyTo({
     //     pitch: Cesium.Math.toRadians(-15.0),
     // }
 });
+
+function createModel(url) {
+    var entity = viewer.entities.add({
+        position: Cesium.Cartesian3.fromDegrees(-75.1836474718837, 39.9675302094665, 30),
+        model: {
+            uri: url,
+            scale: 1
+        },
+    });
+    viewer.trackedEntity = entity;
+}

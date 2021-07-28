5c6dfb1b2731a23bbb2c02a13c993aadb8a9bc9c
Rodrigo Andrade <53790568+randrade8311@users.noreply.github.com>
2021-07-27 17:43:33 -0700

    Added sketchfab integration to main branch (#30)

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { skyLayer, modelLayer } from './src/layers.js';
+import SketchfabIntegration from "./SketchfabIntegration.js";
+
+$("#authenticate").on("click", authenticateUser);
+$("#submitLink").on("click", getSketchfabModelUrl);
+$("#linkSketchfab").hide();
+$("#submitLink").hide();
+
+let modelgltfURL;
+
+const sketchfabIntegration = new SketchfabIntegration();
+sketchfabIntegration.checkToken();
+
+function authenticateUser() {
+  sketchfabIntegration.authenticate();
+  // After authentication, check for token 
+  sketchfabIntegration.checkToken();
+  // Token hardcoced for testing purposes
+  // sketchfabIntegration.token = "pPstovaqjLqqLFx3jRgnXl7ScdTM1V";
+  // sketchfabIntegration.checkToken();
+  if (sketchfabIntegration.token != null) {
+    $("#linkSketchfab").show();
+    $("#submitLink").show();
+  }
+  // console.log("Authenticating");
+}
+
+async function getSketchfabModelUrl() {
+  let modelURL = $("#linkSketchfab").val();
+  console.log(modelURL);
+  // Contains blob url
+  // Fetch model will load model from sketchfab link
+  modelgltfURL = await sketchfabIntegration.fetchModel(modelURL);
+  // readzip only have link to glb file inside zip folder
+  // modelgltfURL = await sketchfabIntegration.readZip("/static/Assets/electricalBox.glb.zip");
+  // console.log(modelgltfURL);
+  // create model will create the model in cesium and display it 
+  console.log(modelgltfURL);
+  console.log("creating model");
+  // Create Model Function - "HERE"
+  // {
+  // This code will load models if stored in a static folder as glbs which has the requirements for Cesium to load files
+  // createModel("/static/Assets/electricalBox.glb");
+  // }
+}
+
 
 let modelArray = [];
 
